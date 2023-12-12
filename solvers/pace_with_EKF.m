a24acb9ace7a9e1797523ba090043149dc329f5a
lopenguin <lorenzo@caltech.edu>
2023-12-12 10:30:27 -0500

    fixed pace for world version

--- solvers/pace_with_EKF.m
+++ solvers/pace_with_EKF.m
@@ -11,6 +11,23 @@ function soln = pace_with_EKF(problem, path)
 % 
 % Lorenzo Shaikewitz for SPARK Lab
 
+%% Define covariances and noise
+% state covariance
+covar_position = problem.covar_position';
+covar_velocity = problem.covar_velocity';
+covar_state = reshape([covar_position(1:3); covar_velocity(1:3)], 6,1);
+covar_state = diag(covar_state);
+
+% process noise (noise added to const. vel. model)
+% TODO: this maybe makes less sense
+processNoise = repmat([0,0.01^2],1,3);
+processNoise = diag(processNoise);
+
+% Measurement noise (todo: this isn't quite right)
+measureNoise = repmat(problem.noiseSigmaSqrt^2, 1,3);
+measureNoise = diag(measureNoise);
+
+
 %% Run PACE at each time step
 soln_pace = [];
 for l = 1:problem.L
@@ -29,17 +46,20 @@ L = size(p,2);
 
 %% Fuse with EKF
 % Create EKF
-% Fixed prior at origin
+% Prior: initial position/velocity
 prior_pos = p(:,2);
 prior_vel = (p(:,2)-p(:,1))/problem.dt;
+
 prior_state = reshape([prior_pos'; prior_vel'],6,1);
 
-% Linear velocity EKF
-EKF_lin = trackingEKF(@constvel,@cvmeas,prior_state, ...
-    'StateTransitionJacobianFcn',@constveljac, ...
-    'MeasurementJacobianFcn',@cvmeasjac);
+% Define EKF
+% EKF = trackingEKF(@constvel,@cvmeas,prior_state, ...
+%     'StateTransitionJacobianFcn',@constveljac, ...
+%     'MeasurementJacobianFcn',@cvmeasjac);
 
-% 
+EKF = trackingEKF(State=prior_state,StateCovariance=covar_state, ...
+        StateTransitionFcn=@constvw,ProcessNoise=processNoise, ...
+        MeasurementFcn=@measureModel,MeasurementNoise=measureNoise);
 
 % Smooth measurements with EKF
 p_smoothed = zeros(3,1,L);
@@ -49,9 +69,9 @@ v_smoothed = zeros(3,1,L);
 v_smoothed(:,:,2) = prior_vel;
 for l = 3:L
     % prediction
-    [xpred, Ppred] = predict(EKF_lin,problem.dt);
+    [xpred, Ppred] = predict(EKF,problem.dt);
     % correction
-    [xcorr, Pcorr] = correct(EKF_lin,p(:,l));
+    [xcorr, Pcorr] = correct(EKF,p(:,l));
 
     % save
     xcorr = reshape(xcorr,2,3);
@@ -64,4 +84,24 @@ soln.p_smoothed = p_smoothed;
 soln.v_smoothed = v_smoothed;
 soln.p_raw = reshape(p,[3,1,L]);
 
+end
+
+%% EKF State Functions
+% compute next state from current state
+function stateNext = constvw(state,dt)
+    % No noise constant velocity and turn rate
+    % State: [x, vx, y, vy, z]
+    F = [1 dt 0  0 0  0; 
+            0  1 0  0 0  0;
+            0  0 1 dt 0  0;
+            0  0 0  1 0  0;
+            0  0 0  0 1 dt;
+            0  0 0  0 0  1;];
+    stateNext = F*state;
+end
+
+% Compute nominal measurement from state
+function z = measureModel(state)
+    % Measurement is just x, y, z position
+    z = [state(1), state(3), state(5)];
 end
