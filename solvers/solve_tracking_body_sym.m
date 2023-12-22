7411a926b210c7bc338d4c3678df7399fc3a9c5c
lopenguin <lorenzo@caltech.edu>
2023-12-21 23:44:52 -0500

    added redundancy term

--- solvers/solve_tracking_body_sym.m
+++ solvers/solve_tracking_body_sym.m
@@ -1,4 +1,4 @@
-function soln = solve_tracking_body(problem)
+function soln = solve_tracking_body_sym(problem)
 % Solves const. vel. (body frame) optimization exactly via SDP
 %   Assume the *body frame* velocity is constant and object is spinning.
 %   Result: spiral trajectory.
@@ -46,8 +46,8 @@ end
 
 %% Define objective
 % optimization vector
-d = 9*(2*L - 1) + 3*L + 3*(L-1); % 2L - 1 rotations, L rotated positions, L-1 body velocities
-% 2L-1 rotations: L rotations, L-1 delta rotations
+d = 9*(2*L - 1) + 3*L + 3*(L-1); % 2L - 1 rotations, 3L rotated positions, 3L-1 body velocities
+% 2L - 1 rotations: L rotations, L-1 delta rotations
 x = msspoly('x',d);
 
 % pull out individual variables
@@ -66,153 +66,51 @@ for l = 1:L
     end
 end
 
-% % define eliminated variables
-% % SHAPE
-% cbar = ones(K,1) / K;
-% sumh = msspoly(zeros(3*N,1));
-% sumW = zeros(N,1);
-% for l = 1:L
-%     for i = 1:N
-%         sumh(ib3(i), :) = sumh(ib3(i), :) + W(i,l)*( ...
-%                           R(ib3(l),:)'*y(ib3(i),l) ...
-%                           - s(ib3(l)));
-% 
-%         sumW(i) = sumW(i) + W(i,l);
-%     end
-% end
-% sumW = diag(reshape(repmat(sumW,1,3)',3*N,1));
-% H = [2*((B'*sumW*B) + lambda*eye(K)), ones(K,1);
-%      ones(1,K), 0];
-% b = [2*B'*sumh + 2*lambda*cbar; 1];
-% invH = H \ b;
-% 
-% c = invH(1:(end-1),:);
-% 
-% % MAIN OPTIMIZATION
-% prob_obj = 0;
-% for l = 1:L
-%     for i = 1:N
-%         obj2 = R(ib3(l),:)'*y(ib3(i),l) - B(ib3(i),:)*c - s(ib3(l));
-%         prob_obj = prob_obj + W(i,l) * (obj2' * obj2);
-%     end
-% end
-% % c regularization
-% prob_obj = prob_obj + lambda*((c - cbar)'*(c - cbar));
-% for l = 2:L-1
-%     % delta v
-%     delv = v(ib3(l)) - v(ib3(l-1));
-%     prob_obj = prob_obj + wv(l-1)*(delv'*delv);
-%     % dR
-%     deldR = reshape(dR(ib3(l),:) - dR(ib3(l-1),:),9,1);
-%     prob_obj = prob_obj + wd(l-1)*(deldR'*deldR);
-% end
-
-%% Symbol-free version
-Y = reshape(y,[3,N,L]);
-P = kron(eye(L),vecRt_r);
-
-% SHAPE (VERIFIED)
+% define eliminated variables
+% SHAPE
 cbar = ones(K,1) / K;
-
-% schur complement inverse
+sumh = msspoly(zeros(3*N,1));
 sumW = zeros(N,1);
 for l = 1:L
     for i = 1:N
+        sumh(ib3(i), :) = sumh(ib3(i), :) + W(i,l)*( ...
+                          R(ib3(l),:)'*y(ib3(i),l) ...
+                          - s(ib3(l)));
+
         sumW(i) = sumW(i) + W(i,l);
     end
 end
 sumW = diag(reshape(repmat(sumW,1,3)',3*N,1));
-H11 = 2*((B'*sumW*B) + lambda*eye(K));
-invH11 = inv(H11);
-H12 = ones(K,1);
-invS = inv(-H12'*invH11*H12);
-G = invH11 + invH11*H12*invS*H12'*invH11;
-g = -invH11*H12*invS;
-% test:
-% c2 = G*(2*B'*sumh + 2*lambda*cbar) + g;
+H = [2*((B'*sumW*B) + lambda*eye(K)), ones(K,1);
+     ones(1,K), 0];
+b = [2*B'*sumh + 2*lambda*cbar; 1];
+invH = H \ b;
 
-% simplify shape
-ycr_coeff = zeros(3*N,9*L);
-ycs_coeff = kron(W,eye(3));
+c = invH(1:(end-1),:);
+
+% MAIN OPTIMIZATION
+prob_obj = 0;
 for l = 1:L
-    dw = reshape(repmat(W(:,l)',3,1),3*N,1);
-    ycr_coeff(:,(9*(l-1)+1):(9*l)) = diag(dw)*kron(Y(:,:,l)',eye(3));
+    for i = 1:N
+        obj2 = R(ib3(l),:)'*y(ib3(i),l) - B(ib3(i),:)*c - s(ib3(l));
+        prob_obj = prob_obj + W(i,l) * (obj2' * obj2);
+    end
 end
-
-Cr = 2*G*B'*ycr_coeff*P;
-Cs = 2*G*B'*ycs_coeff;
-gbar = 2*lambda*G*cbar+g;
-% test:
-% c2 = Cr*r - Cs*s + gbar;
-
-% MEASUREMENT TERM (VERIFIED)
-rar_coeff = zeros(3*N*L,3*L);
-car_coeff = zeros(3*N*L,K);
-sas_coeff = zeros(3*N*L,3*L);
-for l = 1:L
-    dw = reshape(repmat(W(:,l)',3,1),3*N,1).^(1/2);
-
-    rar_coeff((3*N*(l-1)+1):(3*N*l),(9*(l-1)+1):(9*l)) = ...
-           diag(dw)*kron(Y(:,:,l)',eye(3));
-
-    car_coeff((3*N*(l-1)+1):(3*N*l),:) = diag(dw)*B;
-
-    sas_coeff((3*N*(l-1)+1):(3*N*l),(3*(l-1)+1):(3*l)) = ...
-        ycs_coeff(:,(3*(l-1)+1):(3*l));
+% c regularization
+prob_obj = prob_obj + lambda*((c - cbar)'*(c - cbar));
+for l = 2:L-1
+    % delta v
+    delv = v(ib3(l)) - v(ib3(l-1));
+    prob_obj = prob_obj + wv(l-1)*(delv'*delv);
+    % dR
+    deldR = reshape(dR(ib3(l),:) - dR(ib3(l-1),:),9,1);
+    prob_obj = prob_obj + wd(l-1)*(deldR'*deldR);
 end
-
-Ar = rar_coeff*P - car_coeff*Cr;
-As = -(sas_coeff - car_coeff*Cs);
-Ag = -car_coeff*gbar;
-% test
-% meas_term = (Ar*r + As*s + Ag)'*(Ar*r + As*s + Ag);
-
-% SHAPE REGULARIZATION TERM
-if (lambda > 0)
-    Ar = [Ar;  sqrt(lambda)*Cr];
-    As = [As; -sqrt(lambda)*Cs];
-    Ag = [Ag;  sqrt(lambda)*(gbar - cbar)];
-end
-
-% VELOCITY TERM
-dwv = reshape(repmat(wv',3,1),3*(L-2),1).^(1/2);
-eye3LL = [zeros(3*(L-2),3), eye(3*(L-2))];
-eye3LR = [eye(3*(L-2)), zeros(3*(L-2),3)];
-
-Av = diag(dwv)*(eye3LL - eye3LR);
-
-% ROTATION RATE TERM
-dwd = reshape(repmat(wd',9,1),9*(L-2),1).^(1/2);
-eye9LL = [zeros(9*(L-2),9), eye(9*(L-2))];
-eye9LR = [eye(9*(L-2)), zeros(9*(L-2),9)];
-
-Ad = diag(dwd)*(eye9LL - eye9LR);
-
-% ALL TOGETHER (VERIFIED)
-Q = zeros(size(Ag,1) + size(Ad,1) + size(Av,1),d+1);
-% measurements
-Q(1:size(Ag,1),1:(1+9*L+9*(L-1)+3*L)) = ...
-    [Ag, Ar, zeros(size(Ag,1),9*(L-1)), As];
-% rotation rate
-Q((size(Ag,1) + 1):(size(Ag,1)+size(Ad,1)),...
-  (1+9*L+1):(1+9*L+9*(L-1))) = ...
-    Ad;
-% velocity
-Q((size(Ag,1)+size(Ad,1) + 1):(size(Ag,1)+size(Ad,1) + size(Av,1)),...
-  (1+9*L+9*(L-1)+3*L+1):(1+21*L-9+3*(L-1))) = ...
-    Av;
-prob_obj = [1;x]'*(Q'*Q)*[1;x];
-c = Cr*r - Cs*s + gbar;
 
 %% Define constraints
-if ~problem.genconstraints
-    % this runs much faster
-    load data/constraints.mat h g
-else
-% regenerate constraints
-
 % EQUALITY
 h = [];
+
 % SO(3) constraints
 for l = 1:L
     c1 = so3_constraints( R(ib3(l),:));
@@ -253,17 +151,11 @@ g_v = vBoundSq*L - v'*v;
 
 % c bound (0<=c<=1)
 cBoundSq = 1.0; % should just be 1
-g_c = [cBoundSq - c'*c;c]; % includes measurements
+g_c = [cBoundSq - c'*c;c];
 
 % g = [g_c];
 % g = [g_c;g_v];
-% g = [g_s_first; g_v; g_c]; % only use if regenerating each time.
-g = [g_s_first; g_v];
-
-save("data/constraints.mat","g","h");
-end
-
-
+g = [g_s_first; g_v; g_c];
 
 %% Complete problem definition
 problem.vars = x;
@@ -283,7 +175,9 @@ prob = convert_sedumi2mosek(SDP.sedumi.At,...
                             SDP.sedumi.c,...
                             SDP.sedumi.K);
 addpath(genpath(mosekpath))
-[~,res] = mosekopt('minimize info echo(0)',prob);
+param = struct();
+param.MSK_IPAR_LOG = 0;
+[~,res] = mosekopt('minimize info',prob,param);
 [Xopt,yopt,Sopt,obj] = recover_mosek_sol_blk(res,SDP.blk);
 rmpath(genpath(mosekpath))
 soln.solvetime = toc;
@@ -304,13 +198,9 @@ Rs = projectRList(rs);
 drs = x_est((9*L+1):(18*L-9));
 dRs = projectRList(drs);
 
-% s_est = reshape(full(dmsubs(s,x,x_est)),[3,1,L]);
-s_est = reshape(x_est((18*L-9+1):(18*L-9+3*L)),[3,1,L]);
-% v_est = reshape(full(dmsubs(v,x,x_est)),[3,1,L-1]);
-v_est = reshape(x_est((18*L-9+3*L+1):(21*L-9+3*L-3)),[3,1,L-1]);
-% c_est = full(dmsubs(c,x,x_est));
-c_est = Cr*reshape(Rs,9*L,1,1) - Cs*reshape(s_est,3*L,1,1) + gbar;
-% c = Cr*r - Cs*s + gbar;
+s_est = reshape(full(dmsubs(s,x,x_est)),[3,1,L]);
+v_est = reshape(full(dmsubs(v,x,x_est)),[3,1,L-1]);
+c_est = full(dmsubs(c,x,x_est));
 
 % estimate p from s
 p_est = zeros(3,1,L);
@@ -319,7 +209,7 @@ for l = 1:L
 end
 
 % suboptimality gap
-x_proj = [1.0];
+x_proj = [];
 for l = 1:L
     r_temp = reshape(Rs(:,:,l),9,1);
     x_proj = [x_proj; r_temp];
@@ -330,10 +220,9 @@ for l = 1:L-1
 end
 x_proj = [x_proj; reshape(s_est,[3*L,1,1]); reshape(v_est,[3*L-3,1,1])];
 
-% compute gap
-% obj_est = dmsubs(prob_obj,x,x_proj); % slow
-obj_est = x_proj'*(Q'*Q)*x_proj;
-gap = (obj_est - obj(1)) / obj_est;
+% compute gap (THIS TAKES FOREVER)
+% obj_est = dmsubs(prob_obj,x,x_proj);
+% gap = (obj_est - obj(1)) / obj_est;
 
 % compute residuals (TODO: USE WHOLE OBJECTIVE?)
 residuals = zeros(N, L);
@@ -364,9 +253,9 @@ soln.s_est = s_est;
 soln.R_est = Rs;
 soln.dR_est = dRs;
 
-soln.gap = gap;
-soln.x_proj = x_proj;
-soln.obj_est = obj_est;
+% soln.gap = gap;
+% soln.x_proj = x_proj;
+% soln.obj_est = obj_est;
 
 soln.residuals = residuals;
 
